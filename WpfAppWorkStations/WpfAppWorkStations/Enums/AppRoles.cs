51fbc658c4a72f21e88b735f1551cec326fa09bb
jecsonM <eugene.moiseiev@gmail.com>
2026-04-04 12:47:18 +0300

    Делаю курсовую

--- WpfAppWorkStations/WpfAppWorkStations/Enums/AppRoles.cs
+++ WpfAppWorkStations/WpfAppWorkStations/Enums/AppRoles.cs
@@ -4,7 +4,7 @@
     {
         None = 0,
         Master,
-        Buchgalter,
+        Accountant,
         SysAdmin,
         Director
 
---
+++ WpfAppWorkStations/WpfAppWorkStations/ViewModels/ButtonWithAccessViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfAppWorkStations.Enums;
+
+namespace WpfAppWorkStations.ViewModels
+{
+    public class ButtonWithAccessViewModel : BaseViewModel
+    {
+        public ButtonWithAccessViewModel(ICommand command, AppRoles[] allowedRoles)
+        {
+            Command = command;
+            AllowedRoles = allowedRoles;
+        }
+
+        public ICommand Command { get; }
+        public AppRoles[] AllowedRoles { get; }
+    }
+}
